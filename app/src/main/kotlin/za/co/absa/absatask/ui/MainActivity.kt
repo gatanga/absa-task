8777d3234d9f7d1aa89e1a7fe5376dc94fada184
Jean-Marie Nkurunziza <jmvnkuru@gmail.com>
2022-12-07 07:19:44 +0200

    Update MainActivity.kt

--- app/src/main/kotlin/za/co/absa/absatask/ui/MainActivity.kt
+++ app/src/main/kotlin/za/co/absa/absatask/ui/MainActivity.kt
@@ -15,9 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
-
         createViewPagerWithTabs()
     }
 
